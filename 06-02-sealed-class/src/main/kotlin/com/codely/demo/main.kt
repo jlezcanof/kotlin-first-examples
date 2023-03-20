ea65dd6ba8fd22a17a8359fcd60c0abdb247eb92
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2023-03-20 20:43:24 +0100

    comentarios importantes para entender las sealed class

--- 06-02-sealed-class/src/main/kotlin/com/codely/demo/main.kt
+++ 06-02-sealed-class/src/main/kotlin/com/codely/demo/main.kt
@@ -16,8 +16,8 @@ import com.codely.demo.shared.Writer
 fun main() {
     try {
         CatCreator(Reader(), Writer(), Clock(), MapCatRepository()).create()
-    } catch (e: CatCreationException) {
-        when (e) {
+    } catch (e: CatCreationException) { // sealed class
+        when (e) { // match expression
             is InvalidBirthDate -> println("Upss looks like the birth date was invalid, please remember to use the correct format")
             is InvalidColor -> println("Upss looks like the color was invalid, the available colors are: BLACK, RED, CINNAMON, BLUE, CREAM, LILAC, FAWN, WHITE")
             is InvalidId -> println("Upss looks like the id was invalid, please remember to use a uuid next time")
