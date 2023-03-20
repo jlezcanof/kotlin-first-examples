4e80dde3255c2ff3130b8e08e9092cfe7de57588
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2023-03-20 21:43:38 +0100

    funcionamiento de keyword object

--- 07-01-refactor/src/main/kotlin/com/codely/demo/shared/AgeCalculator.kt
+++ 07-01-refactor/src/main/kotlin/com/codely/demo/shared/AgeCalculator.kt
@@ -3,6 +3,6 @@ package com.codely.demo.shared
 import java.time.LocalDate
 import java.time.Period
 
-object AgeCalculator {
+object AgeCalculator { // singleton donde tenemos funciones  todo dentro pero sin tener que hacer new, instancia
     fun calculate(birthDate: LocalDate, now: LocalDate) = Period.between(birthDate, now)
 }
