550c1adaff1106a159265a22598916142698a326
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2023-03-20 16:05:07 +0100

    refactor variables

--- 03-02-scope-functions/src/main/kotlin/com/codely/demo/main.kt
+++ 03-02-scope-functions/src/main/kotlin/com/codely/demo/main.kt
@@ -6,27 +6,29 @@ import kotlin.system.exitProcess
 
 fun main() {
     println("Please enter a date with the format <yyyy-MM-dd>")
-    val date = supportNullableString(readLine())
+    val inputDate = supportNullableString(readLine())
         .takeUnless { // solo validamos que el input no es nulo ni vacio
-            it.isNullOrEmpty() || it.isNullOrBlank()
+            dateIsInformed(it)
         }?.let {
             LocalDate.parse(it)
         }
 
-    if (date == null) {
+    if (inputDate == null) {
         println("The introduced date is null")
         exitProcess(1)
     } else {
-        date.also { // y ademas, metodo side-effect
+        inputDate.also { // y ademas, metodo side-effect
             println("You wrote the date $it")
         }
         val currentDate = LocalDate.now()
-        with(Period.between(date, currentDate).years) {
-            println("The difference between the date you wrote an today is $this years")
+        with(Period.between(inputDate, currentDate)) {
+            println("The difference between the date you wrote an today is ${this.years} years")
         }
     }
 
     println("Bye!")
 }
+
+private fun dateIsInformed(it: String?) = it.isNullOrEmpty() || it.isNullOrBlank()
 
 private fun supportNullableString(line: String?) = line
